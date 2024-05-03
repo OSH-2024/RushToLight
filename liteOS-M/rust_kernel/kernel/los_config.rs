96069281b955c05340ca2c9624418ba21c6430c5
JiabinO <154659312+JiabinO@users.noreply.github.com>
2024-05-04 00:12:11 +0800

    Create los_config.rs

---
+++ liteOS-M/rust_kernel/kernel/los_config.rs
@@ -0,0 +1,16 @@
+pub const LOSCFG_SYS_HEAP_SIZE: u32 = 65536;
+pub type UINT8 = ::std::os::raw::c_uchar;
+pub type UINT16 = ::std::os::raw::c_ushort;
+pub type UINT32 = ::std::os::raw::c_uint;
+pub type INT8 = ::std::os::raw::c_schar;
+pub type INT16 = ::std::os::raw::c_short;
+pub type INT32 = ::std::os::raw::c_int;
+pub type FLOAT = f32;
+pub type DOUBLE = f64;
+pub type CHAR = ::std::os::raw::c_char;
+pub type UINT64 = ::std::os::raw::c_ulonglong;
+pub type INT64 = ::std::os::raw::c_longlong;
+pub type UINTPTR = ::std::os::raw::c_uint;
+pub type INTPTR = ::std::os::raw::c_int;
+pub type Atomic = INT32;
+pub type Atomic64 = INT64;
